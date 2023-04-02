68a101d0e8bf1b3e80511cffec3df2793a59f420
Mateusz Woźniak <mw54544@gmail.com>
2023-04-02 11:57:31 +0200

    function added

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,19 @@
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-let colorName = document.querySelector(".button__colorName")
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
+{
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const colorName = document.querySelector(".button__colorName")
+        body.classList.toggle("dark");
+        colorName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+    };
+    const welcome = () => {
+        console.log("Witam serdecznie")
+    }
+    
+    const init = () => {
+        const changeBeckgroundClick = document.querySelector(".button"); 
+        changeBeckgroundClick.addEventListener("click", toggleBackground);
 
-    colorName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
-}); 
-console.log("Witam serdecznie")
+        welcome();
+    }
+    init();
+}
