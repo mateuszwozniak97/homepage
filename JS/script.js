1a9bf635ae463f1410d28596431ef2a397f6f0d9
Mateusz Woźniak <mw54544@gmail.com>
2023-04-03 10:30:39 +0200

    semicolon added

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,19 @@
 {
     const toggleBackground = () => {
         const body = document.querySelector(".body");
-        const colorName = document.querySelector(".button__colorName")
+        const colorName = document.querySelector(".button__colorName");
         body.classList.toggle("dark");
         colorName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
     };
     const welcome = () => {
-        console.log("Witam serdecznie")
-    }
+        console.log("Witam serdecznie");
+    };
     
     const init = () => {
         const changeBeckgroundClick = document.querySelector(".button"); 
         changeBeckgroundClick.addEventListener("click", toggleBackground);
 
         welcome();
-    }
+    };
     init();
-}
+};
